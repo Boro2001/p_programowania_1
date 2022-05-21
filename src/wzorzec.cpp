cbe851630aa7efa061d7650c6007c25babfbb2c4
Mikołaj Borowicz <62395530+Boro2001@users.noreply.github.com>
2022-05-21 19:13:29 +0200

    Co-authored-by: xXSugarBabyXx <xXSugarBabyXx@users.noreply.github.com>

--- src/wzorzec.cpp
+++ src/wzorzec.cpp
@@ -13,31 +13,33 @@ void print_table(char ** tablica, int table_width, int table_height){
         cout << endl;
     }
 }
-char ** make_table(int table_width, int table_height){
-    char ** tablica = new char*[table_height];
-    for(int i = 0; i < table_height; ++i){
-        tablica[i] = new char[table_width];
+char ** make_table(int matrix_size){
+    char ** tablica = new char*[matrix_size];
+    for(int i = 0; i < matrix_size; ++i){
+        tablica[i] = new char[matrix_size];
+        for(int j = 0; j < matrix_size; ++j){
+            tablica[i][j] = ' ';
+        }
     }
     return tablica;
 }
-char ** flip_table(char ** tablica, int table_width, int table_height){
-    char ** flipped_table = new char * [table_width]; 
-    for(int i = 0; i < table_width; ++i) flipped_table[i] = new char[table_height];
-
-    for(int i = 0; i < table_width; ++i){
-        for(int j = 0; j < table_height; ++j){
-            flipped_table[i][j] = tablica[table_height - j - 1][i];
+char ** flip_table(char ** table, int matrix_size){
+    char ** flipped_table = new char*[matrix_size];
+    for(int i = 0; i < matrix_size; ++i){
+        flipped_table[i] = new char[matrix_size];
+        for(int j = 0; j < matrix_size; ++j){
+            flipped_table[i][j] = table[j][i];
         }
     }
     return flipped_table;
 }
-int find_pattern(char ** tablica, int table_width, int table_height, string pattern, bool flipped)
+int find_pattern(char ** tablica, int size, string pattern, bool flipped)
 {
     int count = 0;
     int pattern_length = pattern.length();
-    print_table(tablica, table_width, table_height);
-    for(int i = 0; i < table_height; ++i){
-        for(int j = 0; j < table_width - pattern_length + 1; ++j){
+    print_table(tablica, size, size);
+    for(int i = 0; i < size; ++i){
+        for(int j = 0; j < size - pattern_length + 1; ++j){
             bool found = true;
             for(int k = 0; k < pattern_length; ++k){
                 if(tablica[i][j + k] != pattern[k]){
@@ -49,29 +51,27 @@ int find_pattern(char ** tablica, int table_width, int table_height, string patt
                 if(!flipped)
                     {++count; cout << "found at " << i << " " << j << endl;}
                 else
-                    {++count; cout << "found at " << table_height - j - 1 << " " << i << endl;}
+                    {++count; cout << "found at " << size - j - 1 << " " << i << endl;}
                 }
         }
     }
     return count;
 }
-int count_occurances_in_file(string file_name, string pattern, int x, int y)
+int count_occurances_in_file(string file_name, string pattern, int size)
 {
     char c; int n = 0;
-    char ** tablica = make_table(x, y);
+    char ** tablica = make_table(size);
 
     FILE *file = fopen(file_name.c_str(), "r");
     if(file == NULL) return 0;
     while((c = fgetc(file)) != EOF)
         if(c!='\n'){
-            tablica[n/x][n%x] = c;
+            tablica[n/size][n%size] = c;
             ++n;
         }
     fclose(file); 
 
-    return find_pattern(tablica, x, y, pattern, false) + find_pattern(flip_table(tablica, x, y), y, x, pattern, true);
-
-
+    return find_pattern(tablica, size, pattern, false) + find_pattern(flip_table(tablica, size), size, pattern, true);
     return 0;
 }
 
@@ -89,7 +89,8 @@ int main(int argc, char * argv[])
     cout << "file: " << file_name << endl;
     cout << "table_width: " << table_width << endl;
     cout << "table_height: " << table_height << endl;
+    int max_size = (table_width > table_height) ? table_width : table_height;
 
-    int count = count_occurances_in_file(file_name, pattern, table_width, table_height);
+    int count = count_occurances_in_file(file_name, pattern, max_size);
     return 0;
 }
